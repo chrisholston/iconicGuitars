4b6bb50817e856f24104d37cb8c18de8836c9174
Chris Holston <chrisholston@me.com>
2020-11-16 21:51:43 -0600

    connected to mongoDB

--- config/database.js
+++ config/database.js
@@ -1,36 +1,23 @@
-// TODO: INPUT OUR MONGODB URI AND SET UP EVENT LISTENER
-
-// set up our database
-// mongodb+srv://admin:1234@cluster0.wyypc.mongodb.net/iconicGuitars?retryWrites=true&w=majority
-
-
-//  set up our database with the connection URI from mongoDB. We change the link and put in our password as well as the project name.
-
-// REMEMBER to install the mongoose module (npm i mongoose)
-
-
 // 1ST STEP IS REQUIRE OUR MODULES (MONGOOSE)
-// mongodb+srv://admin:1234@cluster0.wyypc.mongodb.net/iconicGuitars?retryWrites=true&w=majority
+const mongoose =require('mongoose');
 
-const mongoose = require('mongoose');
-// 2. SET UP OUR SHORTCUT VARIABLE FOR CONVENIENCE - we will declare db and assign it to the property of connection. This is a property on the mongoose object that reprsents the actual connection on mongoose db. This will have information that 
+// set up shortcut variables
+
 const db = mongoose.connection;
 
-// 3. CONNECT TO MONGO DB
-// const connectionURI= 'mongodb+srv://admin:1234@cluster0.wyypc.mongodb.net/iconicGuitars?retryWrites=true&w=majority';
+const connectionURI= "mongodb+srv://admin:1234@cluster0.wyypc.mongodb.net/iconicGuitars?retryWrites=true&w=majority";
 
-// 3. CONNECT TO MONGO DB- **We paste our mongodb connection URI into this string.** We add 3 options-
+// 2. CONNECT TO MONGO DB- **We paste our mongodb connection URI into this string.** We add 3 options-
 //  useCreateIndex, useNewUrlParser, useUnifiedTopology.
-// 
-/*mongoose.connect(connectionURI, {
+
+mongoose.connect(connectionURI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
 
  });
-*/
 
 //4. SET UP A LISTENER TO ALERT US WHEN WERE CONNECTED
-// //db.on('connected', function(){
-// console.log(`connected to MongoDB on ${db.host}:${db.port}`);
-// });
+db.on('connected', function(){
+console.log(`connected to MongoDB on ${db.host}:${db.port}`);
+});
--- models/guitar.js
+++ models/guitar.js
@@ -1,6 +1,34 @@
-// const guitar = new guitar {
-//     model: String,
-//     year: Number,
-//     Genre: String,
-//     Story: String
-// }
+// require our modules
+const mongoose = require('mongoose');
+
+
+// set up shortcut variable
+const Schema = mongoose.Schema;
+
+//initialize guitarSchema 
+
+const guitarSchema = new Schema ({
+    Brand: {
+        type: String,
+        enum: ['Gibson', 'Fender', 'Gretsch']
+    },
+    Model: {
+        type: String
+    },
+    Genre: {
+        type: String,
+        enum:['Blues','Country','Rock']
+    },
+    Year: {
+        type: Number
+    },
+    Story: {
+        type: String
+    },
+    Image: {
+        type: String
+    },
+});
+
+// NEXT STEP- go into mongoDB terminal, guitar.Create(Model:Fender)
+
